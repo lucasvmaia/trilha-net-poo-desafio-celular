83e6ccc640bc303bf7eb61ca93dd9898b88dfee2
Lucas Maia <lucasvaz.maialvm@gmail.com>
2024-08-18 22:03:11 -0300

    Desafio POO celular C#

--- Program.cs
+++ Program.cs
@@ -1,3 +1,12 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine("Smartphone Nokia");
+Smartphone nokia = new Nokia(numero: "(11) 12345-6789", modelo: "Nokia Modelo 1", imei: "11111", memoria: 8);
+nokia.Ligar();
+nokia.InstalarAplicativo("Youtube");
+
+Console.WriteLine("\nSmartphone Iphone");
+Smartphone iphone = new Iphone(numero: "(15) 98745-028596", modelo: "Iphone Modelo 9", imei: "22222", memoria: 128);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Word");
